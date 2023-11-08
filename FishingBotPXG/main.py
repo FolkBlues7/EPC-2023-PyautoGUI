ed80a2440318c4648b9afa12b1e08eea7f7934d2
FolkBlues7 <viniciusgabrielprofissional42@gmail.com>
2023-11-08 16:30:55 -0300

    Solving directory path problems

--- FishingBotPXG/main.py
+++ FishingBotPXG/main.py
@@ -8,20 +8,20 @@ def startFishing(): #throws the bait and push it in the right time
     x, y = pg.center(randomSpot)
     pg.moveTo(x, y)
     key.press_and_release('capslock') #throws the bait
-    while (pg.locateOnScreen("pictures/bubbles.png", region= randomSpot,confidence=0.8) == None):
+    while (pg.locateOnScreen("D:\EPC Github\EPC-2023-PyautoGUI\FishingBotPXG\pictures/bubbles.png", region= randomSpot,confidence=0.8) == None):
         print("waiting for fish")    
     key.press_and_release('capslock') #push the bait
     
 battleListRegion = (1716, 388, 204, 352)
 def checkCreatures(): #checks if there's creatures in the battle list
-    if (pg.locateOnScreen("pictures/magikarp.png", region= battleListRegion, confidence= 0.8) != None):
+    if (pg.locateOnScreen("D:\EPC Github\EPC-2023-PyautoGUI\FishingBotPXG\pictures/magikarp.png", region= battleListRegion, confidence= 0.8) != None):
         return True
     else:
         return False
 
 singleTargetRegion = (1721, 421, 33, 261)  
 def checkTargetedCreature():#check if there's a targeted creature
-    if (pg.locateOnScreen("pictures/redTarget.png", region= singleTargetRegion, confidence= 0.8) != None):
+    if (pg.locateOnScreen("D:\EPC Github\EPC-2023-PyautoGUI\FishingBotPXG\pictures/redTarget.png", region= singleTargetRegion, confidence= 0.8) != None):
         return True
     else:
         return False
@@ -41,7 +41,7 @@ def killSingleTarget():#uses the preset skills and stop if there's no targets
 
 hungryIconRegion = (165, 77, 14, 17)
 def checkHungry(): #gives food for the pokemon when necessary
-    if(pg.locateOnScreen("pictures/hungryIcon.png",region=hungryIconRegion, confidence=0.9) != None):
+    if(pg.locateOnScreen("D:\EPC Github\EPC-2023-PyautoGUI\FishingBotPXG/pictures/hungryIcon.png",region=hungryIconRegion, confidence=0.8) != None):
         key.press_and_release('F3')
     else:
         print("Não está com fome!")
@@ -58,12 +58,13 @@ def solvePuzzle():#solve the fishing minigame
         while True:
             if (checkCreatures() == True):
                 break
-            blueBar = pg.locateOnScreen("pictures/blueBar.png",region=puzzleRegion, confidence=0.9)
-            fishPuzzle = pg.locateOnScreen("pictures/fishPuzzle.png",region=puzzleRegion, confidence=0.8, grayscale=True)
+            blueBar = pg.locateOnScreen("D:\EPC Github\EPC-2023-PyautoGUI\FishingBotPXG\pictures/blueBar.png",region=puzzleRegion, confidence=0.9)
+            fishPuzzle = pg.locateOnScreen("D:\EPC Github\EPC-2023-PyautoGUI\FishingBotPXG\pictures/fishPuzzle.png",region=puzzleRegion, confidence=0.8, grayscale=True)
             while(blueBar == None or fishPuzzle == None):
-                blueBar = pg.locateOnScreen("pictures/blueBar.png",region=puzzleRegion, confidence=0.9)
-                fishPuzzle = pg.locateOnScreen("pictures/fishPuzzle.png",region=puzzleRegion, confidence=0.8, grayscale=True)
-                key.release('space')
+                blueBar = pg.locateOnScreen("D:\EPC Github\EPC-2023-PyautoGUI\FishingBotPXG\pictures/blueBar.png",region=puzzleRegion, confidence=0.9)
+                fishPuzzle = pg.locateOnScreen("D:\EPC Github\EPC-2023-PyautoGUI\FishingBotPXG\pictures/fishPuzzle.png",region=puzzleRegion, confidence=0.8, grayscale=True)
+                #key.release('space')
+                key.press('space')
                 if (checkCreatures() == True):
                     break
             if (checkCreatures() == True):
@@ -74,30 +75,14 @@ def solvePuzzle():#solve the fishing minigame
                 key.release('space')
 
 key.wait('h')
-checkHungry()
-
-#while True:
-    #checkHungry()
-#    checkHeal()
-    #pg.displayMousePosition()
-    #checkHungry()
-#    startFishing()
-#    pg.sleep(1)
-#    solvePuzzle()
-#    pg.sleep(1)
-#    killCreatures()
-#    pg.sleep(1)
-#    key.press_and_release('F12') #autoloot    
-   
-
-
-    
-
-
-
-#while True:
-#    startFishing()
-#    pg.sleep(1) #if it's too fast, won't work
-#    killCreatures()
-#    pg.sleep(1)
-#    key.press_and_release('F12') #autoloot
+while True:
+    pg.sleep(1)
+    checkHungry()
+    pg.sleep(1)
+    startFishing()
+    pg.sleep(1)
+    solvePuzzle()
+    pg.sleep(1)
+    killCreatures()
+    pg.sleep(1)
+    key.press_and_release('F12') #autoloot    
